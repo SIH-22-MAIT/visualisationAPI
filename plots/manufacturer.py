41c8609c3d3b4a882d26930f6140f7d180728241
Ashish Papanai <ashishpapanai@gmail.com>
2022-04-14 17:59:56 +0530

    Added: Daily Data Plots API

--- plots/manufacturer.py
+++ plots/manufacturer.py
@@ -5,7 +5,8 @@ manufacturer_df = pd.read_csv('data/manufacturer.csv')
 
 class Manufacturers():
     def __init__(self):
-        self.NameQuantity = self.DrugNameQuantity();
+        self.NameQuantity = self.DrugNameQuantity()
+        self.DailyData = self.dailyPlot()
     
     def DrugNameQuantity(self):
         meds = manufacturer_df.pivot_table(index = ['drugName'], aggfunc ='size')
@@ -17,3 +18,17 @@ class Manufacturers():
         data = {'x': x, 'y': y}
         return json.dumps(data)
     
+    def dailyPlot(self):
+        daily = manufacturer_df[manufacturer_df.prescription == True] 
+        daily['outTime'] = pd.to_datetime(daily['outTime'],unit='s')
+        daily = daily.pivot_table(index = daily.outTime.dt.date, aggfunc ='size')
+        daily = daily.to_frame()
+        daily = daily.reset_index()
+
+        x = daily['outTime'].to_list()
+        x = [date_obj.strftime('%Y-%m-%d') for date_obj in x]
+        y = daily[0].to_list()
+        daily_data = {'x': x, 'y': y}
+
+        return json.dumps(daily_data)
+            
